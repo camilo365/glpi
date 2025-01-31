4207ed3816870fced7ebc4555d630be686b88c52
camilo365 <juniorcamiloco0124@gmail.com>
2025-01-31 14:44:26 -0500

    conexion bd

---
+++ bd/db.js
@@ -0,0 +1,27 @@
+/* process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; */
+import sql from 'mysql2/promise';
+
+export async function config(){
+
+    console.log(process.env.BD_DATABASE_B)
+    try {
+        const connection = await sql.createConnection({
+            host: process.env.BD_DATABASE_B, 
+            user: process.env.BD_USER_B,      
+            password: process.env.BD_PASSWORD_B, 
+            port: process.env.BD_PORT_B,      
+            database: process.env.BD_NAME_B
+        })
+        console.log('conexion establecida')
+        return connection
+    } catch (error) {
+        console.error('error al conectar', error)
+        console.log()
+        throw error;
+    }
+} 
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'//
+import bodyParser from 'body-parser'; 
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+const port = 3000;
+import { config } from './bd/db.js';
+
+app.use(cors());
+app.use(bodyParser.json());
+
+config();
+
+app.listen(port, () => {
+    console.log(`Servidor de Node.js corriendo en http://localhost:${port}`);
+});
